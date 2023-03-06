600dae9c4e141a0f51d15d6b68f4c7205c34eeff
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-03-06 15:46:10 +0100

    made currencies array

---
+++ src/currencies.js
@@ -0,0 +1,29 @@
+const currencies = [
+    {
+        code: "PLN",
+        name: "Złoty",
+        rate: 1,
+    },
+    {
+        code: "NZD",
+        name: "Dolar nowozelandzki",
+        rate: 2.78,
+    },
+    {
+        code: "USD",
+        name: "Dolar amerykański",
+        rate: 4.43,
+    },
+    {
+        code: "WON",
+        name: "Won koreański",
+        rate: 0.0035,
+    },
+    {
+        code: "EUR",
+        name: "Euro",
+        rate: 4.75,
+    }
+];
+
+export default currencies;
