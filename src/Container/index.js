941cbca8a4adac88f2d952b211bcddf90d3ea6c6
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-03-23 23:42:38 +0100

    added flex container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,44 +1,26 @@
-import { GridContainer, Item, Button } from "./styled";
-import React, { useState } from "react";
-import { CgSun } from "react-icons/cg";
-import { HiMoon } from "react-icons/hi";
+import { GridContainer, Item, Flex, Button, Time } from "./styled";
 
-const Container = ({ formContent, resultContent, timeContent, showResult }) => {
-
-    const [color, setColor] = useState(false);
-    const [icon, setIcon] = useState(false);
-
-    const handleClick = () => {
-        setColor(current => !current);
-        setIcon(current => !current);
-    }
+const Container = ({ formContent, resultContent, timeContent, showResult, buttonContent, mediaTimeContent }) => {
 
     return (
         <GridContainer>
             <Item
                 first
-                style={{
-                    background: color ? 'rgba(211, 197, 153, 0.5)' : 'rgb(41, 39, 39, 0.7)',
-                    color: color ? 'rgb(32, 31, 31)' : 'white',
-                }}
             >
-                <Button
-                    onClick={handleClick}
-                    style={{
-                        color: icon ? 'black' : 'white',
-                        background: icon ? 'white' : 'black'
-                    }}>
-                    {icon ? <HiMoon size={25} /> : <CgSun size={25} />}
+                <Flex>
+                <Button button>
+                    {buttonContent}
                 </Button>
+                <Time time>
+                    {mediaTimeContent}
+                </Time>
+                </Flex>
                 <p>
                     {formContent}
                 </p>
             </Item>
             <Item
-                style={{
-                    background: color ? 'rgba(211, 197, 153, 0.5)' : 'rgb(41, 39, 39, 0.7)',
-                    color: color ? 'rgb(32, 31, 31)' : 'white',
-                }}
+                second
             >
                 <p>
                     {timeContent}
@@ -48,7 +30,7 @@ const Container = ({ formContent, resultContent, timeContent, showResult }) => {
                 showResult ?
                     <Item
                         last
-                        style={{ background: color ? 'rgba(211, 197, 153, 0.5)' : 'rgb(41, 39, 39, 0.7)', color: color ? 'rgb(32, 31, 31)' : 'white', }}
+
                     >
                         {resultContent}
                     </Item>
--- src/Container/styled.js
+++ src/Container/styled.js
@@ -12,8 +12,8 @@ export const GridContainer = styled.div`
 
 export const Item = styled.div`
     padding: 20px;
-    background: rgba(211, 197, 153, 0.8);
-    color: rgb(32, 31, 31);
+    background: ${({ theme }) => theme.bodyColor};
+    color: ${({ theme }) => theme.textColor};
     margin: 10px;
     border-radius: 25px;
     box-shadow: 0px 0px 31px 11px rgba(0, 0, 0);
@@ -23,29 +23,56 @@ export const Item = styled.div`
         text-align: center;
     `}
 
+    ${({second}) => second && css`
+    @media (max-width: 767px) {
+        display: none;
+    }
+    `} 
+    
     ${({last}) => last && css`
     min-height: 70px;
     grid-column-start: 3;
     grid-column-end: 1;
+
+    @media (max-width: 767px) {
+        grid-column-start: 1;
+        grid-column-end: 1;
+    }
     `}
+
+    @media (max-width: 767px) {
+       grid-template-columns: 300px;
+       grid-gap: 20px;
+    }
 `;
 
-export const Button = styled.button`
-    cursor: pointer;
-    border-radius: 50%;
-    border: none;
-    height: 35px;
-    width: 35px; 
-    font-size: 25px;
-    margin-right: 600px;
-    background: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0px 0px 31px 11px rgba(0, 0, 0);
-    transition: 1s;
+export const Flex =styled.div`
+ 
+ @media (max-width: 767px) {
+    display: grid;
+    grid-template-columns: 100px 150px;
 
-    &:hover {
-        transform: scale(1.1);
-    }
+    
+ }
+
+`;
+
+export const Button = styled.div`
+${({button}) => button && css`
+border: none;
+`}
+`;
+
+export const Time = styled.div`
+
+
+${({time}) => time && css`
+
+@media (max-width: 767px) {
+    
+    border-radius: 10px;
+    box-shadow: 0px 0px 31px 11px rgba(0, 0, 0);
+}
+`}
+
 `;
