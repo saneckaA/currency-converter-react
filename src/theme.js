7ea9772a2a3c08d3082fb3cb015694b82c6dfbdb
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-03-23 22:28:30 +0100

    added light and dark theme

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    lightTheme: {
+        bodyColor: "rgba(211, 197, 153, 0.5)",
+        textColor: "rgb(32, 31, 31)",
+        iconColor: "black",
+        buttonColor: "rgb(255, 255, 255, 0.1)",
+    },
+
+    darkTheme: {
+        bodyColor: "rgb(41, 39, 39, 0.7)",
+        textColor: "rgb(255, 255, 255)",
+        iconColor: "white",
+        buttonColor: "rgb(4, 4, 12, 0.1)",
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
